350dab4a3a3961732be41b0ec6577cac7342193f
Evan Bremer <evan.bremer@chrobinson.com>
2019-06-23 15:12:27 -0500

    add jest

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,7 @@
 const addTwoThings = require('./index');
 
-const input = [3,5];
+it('adds two positive integers', () => {
+  const input = [3, 5];
 
-const result = addTwoThings(...input);
-const expectedResult = 8;
-
-if (result !== expectedResult) {
-  throw new Error(`did not get expected result for "addTwoThings". expected ${expectedResult}, but got ${result}`);
-}
-
-// ... ugh, this is PAINFUL
-// we need a way to group tests and catch errors for failures...
-// testing different permutations is going to be a maintenance nightmare
+  expect(addTwoThings(...input)).toBe(8);
+});
