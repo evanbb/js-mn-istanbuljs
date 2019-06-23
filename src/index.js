fba34943593868086d812a8ea6ee6a244a3f049a
Evan Bremer <evan.bremer@chrobinson.com>
2019-06-23 15:15:27 -0500

    remove manual instrumentation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-global.coverageData = global.coverageData || {};
-global.coverageData['src/index.js'] = {
-  functions: {
-    '0': 0
-  },
-  lines: {
-    '0': 0,
-    '1': 0
-  },
-  branches: {
-    // we have no branches here, but other code obviously could
-  }
-};
-
 module.exports = function addTwoThings(x, y) {
-  global.coverageData['src/index.js'].functions[0]++ ; 
-  global.coverageData['src/index.js'].lines[0]++; const result = x + y;
-  global.coverageData['src/index.js'].lines[1]++; return result;
+  const result = x + y;
+  return result;
 };
