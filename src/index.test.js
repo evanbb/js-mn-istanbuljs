061283f272bfb641b3f0ca5c1ca305d3c300d197
Evan Bremer <evan.bremer@chrobinson.com>
2019-06-23 15:26:57 -0500

    init react app

--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-const addTwoThings = require('./index');
-
-it('adds two positive integers', () => {
-  const input = [3, 5];
-
-  expect(addTwoThings(...input)).toBe(8);
-});
--- src/some-consumer.test.js
+++
@@ -1,6 +0,0 @@
-const multiplyTwoThings = require('./some-consumer');
-
-it('multiplies two positive integers', () => {
-  const result = multiplyTwoThings(5, 2);
-  expect(result).toBe(10);
-});
