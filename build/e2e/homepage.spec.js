bab760b8aaa5151f80482046b91cfe1a24f61646
Evan Bremer <evan.bremer@chrobinson.com>
2019-06-23 16:39:11 -0500

    serve app, serve instrumented code, write coverage

--- build/e2e/homepage.spec.js
+++ build/e2e/homepage.spec.js
@@ -1,4 +1,6 @@
 import puppeteer from 'puppeteer';
+import path from 'path';
+import fs from 'fs';
 
 jest.setTimeout(60000);
 
@@ -10,9 +12,40 @@ beforeAll(async () => {
     slowMo: 500
   });
   page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+
+  page.on('request', req => {
+    const requestUrl = req.url();
+    const indexOfStaticJs = requestUrl.indexOf('/static/js/');
+    const isForStaticJs = indexOfStaticJs !== -1;
+
+    if (isForStaticJs) {
+      req.respond({
+        status: 200,
+        contentType: 'application/json',
+        body: fs.readFileSync(path.join(
+          process.cwd(),
+          requestUrl.substring(indexOfStaticJs).replace('/static/js/', '/static-instrumented/js/')
+        ))
+      });
+    } else {
+      req.continue();
+    }
+  });
 });
 
 afterAll(async () => {
+  const coverageData = await page.evaluate('window.__coverage__')
+
+  fs.writeFileSync(
+    path.join(process.cwd(), 'e2e-coverage.json'),
+    JSON.stringify(coverageData),
+    {
+      encoding: 'utf-8'
+    }
+  )
+
   await browser.close();
 });
 
